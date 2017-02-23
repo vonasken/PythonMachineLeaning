d68fc7d4495ff88575c14d67fe21877ca6c97da5
Marc Von Asken <vonasken@gmail.com>
2017-02-23 12:09:20 -0800

    6 line machine learning prgm

--- hello-ml.py
+++ hello-ml.py
@@ -1,3 +1,6 @@
-import sklearn
+ from sklearn import tree
 features = [[140, 0], [130, 0], [150, 1], [170, 1]
-labels = ["apple", "apple", "orange", "orange"]
+labels = [0, 0, 1, 1]
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+print clf.predict([[160, 0]])
